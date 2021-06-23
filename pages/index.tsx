0d6560608ee4b2a5f03531e7f6ec472606f2b645
Tugay Cem Evci <tevci@nuevo.com.tr>
2021-06-23 21:11:29 +0300

    test graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,41 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import { graphql, buildSchema } from "graphql";
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return "Hello world!";
+    },
+};
+
+try {
+    // Run the GraphQL query '{ hello }' and print out the response
+    fetch("http://localhost:9002/graphql", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({ query: `{
+          pets {
+            name
+            image
+          }
+        }
+        ` }),
+    })
+        .then((r) => r.json())
+        .then((data) => console.log("data returned:", data));
+} catch (error) {
+    console.log("error", error);
+}
 
 export default function Home() {
-  return (
-    <div >
-      user list
-    </div>
-  )
+    return <div>user list</div>;
 }
